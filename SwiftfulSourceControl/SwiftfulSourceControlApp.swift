ff93ce4c45c06ad3711fc52e6c79e4b9b27f63c1
Shahzodbek <muhammadjonovshahzodbek@gmail.com>
2026-02-09 11:19:49 +0500

    Initial Commit

---
+++ SwiftfulSourceControl/SwiftfulSourceControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulSourceControlApp.swift
+//  SwiftfulSourceControl
+//
+//  Created by MacBook Pro on 09/02/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulSourceControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
